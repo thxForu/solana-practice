7df71b0dfcd547301ae748554fba12636e4b1260
eugene <48453875+thxForu@users.noreply.github.com>
2024-12-26 16:44:29 +0200

    add transfer spl token.

--- projects/sol_and_apl_transfer_00/tests/token_deposit_01.ts
+++ projects/sol_and_apl_transfer_00/tests/token_deposit_01.ts
@@ -1,14 +1,15 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { TokenDeposit01 } from "../target/types/sol_and_apl_transfer_00";
+import { SolAndSplTransfer00 } from "../target/types/sol_and_spl_transfer_00";
 import { Keypair, LAMPORTS_PER_SOL, } from "@solana/web3.js";
+import { TOKEN_PROGRAM_ID, createMint, createAssociatedTokenAccount, mintTo, getAssociatedTokenAddress } from "@solana/spl-token";
+import { assert } from "chai";
 
-
-describe("sol_and_apl_transfer_00", () => {
+describe("sol_and_spl_transfer_00", () => {
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
 
-  const program = anchor.workspace.TokenDeposit01 as Program<TokenDeposit01>;
+  const program = anchor.workspace.SolAndSplTransfer00 as Program<SolAndSplTransfer00>;
 
   it("transferLamports", async () => {
     const toWallet = Keypair.generate();
@@ -33,5 +34,64 @@ describe("sol_and_apl_transfer_00", () => {
 
     const balance = await provider.connection.getBalance(toWallet.publicKey);
     console.log("New wallet balance:", balance / LAMPORTS_PER_SOL);
+  });
+
+  it("transferSplTokens", async () => {
+    const fromWallet = provider.wallet as anchor.Wallet;
+    const toWallet = Keypair.generate();
+
+    const mint = await createMint(
+      provider.connection,
+      fromWallet.payer,
+      fromWallet.publicKey,
+      null,
+      9
+    );
+
+    const fromAta = await createAssociatedTokenAccount(
+      provider.connection,
+      fromWallet.payer,
+      mint,
+      fromWallet.publicKey
+    );
+
+    const toAta = await createAssociatedTokenAccount(
+      provider.connection,
+      fromWallet.payer,
+      mint,
+      toWallet.publicKey
+    );
+
+      const amount = 10_000_000_000; // 10 tokens
+      await mintTo(
+        provider.connection,
+        fromWallet.payer,
+        mint,
+        fromAta,
+        provider.wallet.publicKey,
+        amount
+      );
+
+    const fromAtaBalance = await provider.connection.getTokenAccountBalance(fromAta);
+    console.log("Initial from balance:", fromAtaBalance.value.uiAmount);
+
+    const transferAmount = amount / 2;
+    await program.methods
+    .transferSplTokens(new anchor.BN(transferAmount))
+    .accounts({
+      from: provider.wallet.publicKey,
+      fromAta,
+      toAta,
+    })
+    .rpc();
+
+    const toAtaBalance = await provider.connection.getTokenAccountBalance(toAta);
+    console.log("Final to balance:", toAtaBalance.value.uiAmount);
+
+    assert.equal(
+      toAtaBalance.value.uiAmount,
+      transferAmount / (1e9), // конвертуємо в UI amount
+      "Incorrect transfer amount"
+    );
   });
 });
