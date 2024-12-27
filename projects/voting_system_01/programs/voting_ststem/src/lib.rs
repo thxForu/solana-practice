176988745961a667d4356afa0bbc2c3d686d247b
eugene <48453875+thxForu@users.noreply.github.com>
2024-12-27 17:09:41 +0200

    add voting for a proposal and tests for it.

--- projects/voting_system_01/programs/voting_ststem/src/lib.rs
+++ projects/voting_system_01/programs/voting_ststem/src/lib.rs
@@ -25,10 +25,79 @@ pub mod voting_system {
 
         Ok(())
     }
+
+    pub fn vote_for(ctx: Context<CreateVote>) -> Result<()> {
+        let proposal = &mut ctx.accounts.proposal;
+        let vote = &mut ctx.accounts.vote;
+        let clock = Clock::get()?;
+
+        if clock.unix_timestamp > proposal.ends_at {
+            return err!(ErrorCode::VotingEnded);
+        }
+
+        
+        vote.voter = ctx.accounts.voter.key();
+        vote.proposal = proposal.key();
+        vote.vote_type = true;
+
+        proposal.votes_for += 1;
+
+        Ok(())
+    }
+
+    pub fn vote_against(ctx: Context<CreateVote>) -> Result<()> {
+        let proposal = &mut ctx.accounts.proposal;
+        let vote = &mut ctx.accounts.vote;
+        let clock = Clock::get()?;
+
+        if clock.unix_timestamp > proposal.ends_at {
+            return err!(ErrorCode::VotingEnded);
+        }
+        
+        vote.voter = ctx.accounts.voter.key();
+        vote.proposal = proposal.key(); 
+        vote.vote_type = false;
+
+        proposal.votes_against += 1;
+
+        Ok(())
+    }
+}
+
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Voting has ended.")]
+    VotingEnded,
+    #[msg("User has already voted.")]
+    AlreadyVoted,
+}
+
+
+#[account]
+pub struct Vote {
+    pub voter: Pubkey,
+    pub proposal: Pubkey,
+    pub vote_type: bool,
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreateVote<'info> {
+    #[account(mut)]
+    pub proposal: Account<'info, Proposal>,
+    #[account(mut)]
+    pub voter: Signer<'info>,
+    
+    #[account(
+        init,
+        payer = voter,
+        space = 8 + 32 + 32 + 1, // discriminator + voter pubkey + proposal pubkey + bool
+        seeds = [b"vote", proposal.key().as_ref(), voter.key().as_ref()],
+        bump,
+    )]
+    pub vote: Account<'info, Vote>,
+    pub system_program: Program<'info, System>,
+}
 
 #[derive(Accounts)]
 #[instruction(title: String, description: String)]
@@ -36,7 +105,7 @@ pub struct CreateProposal<'info> {
     #[account(
         init, 
         payer = creator,
-        space = 8 + 32 + 4 + title.len() + 4 + description.len() +8 +8 +8
+        space = 8 + 32 + 4 + title.len() + 4 + description.len() + 8 +8 +8
     )]
     pub proposal: Account<'info, Proposal>,
 
--- projects/voting_system_01/tests/voting_ststem.ts
+++ projects/voting_system_01/tests/voting_ststem.ts
@@ -6,51 +6,165 @@ import { expect } from "chai";
 describe("voting_system", () => {
   // Configure the client to use the local cluster.
   const provider = anchor.AnchorProvider.env();
-  const program = anchor.workspace.VotingStstem as Program<VotingSystem>;
+  const program = anchor.workspace.VotingSystem as Program<VotingSystem>;
 
   before(() => {
     anchor.setProvider(provider);
   });
 
-  it("Creates a proposal", async () => {
+  async function createProposal(
+    title: string = "Test Proposal",
+    description: string = "Test Description",
+    duration: number = 24 * 60 * 60
+  ) {
     const proposalKeypair = anchor.web3.Keypair.generate();
 
-    const title = "First Proposal";
-    const description = "This is a test proposal";
-    const duration = 24 * 60 * 60;
+    await program.methods
+      .createProposal(title, description, new anchor.BN(duration))
+      .accounts({
+        proposal: proposalKeypair.publicKey,
+        creator: provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([proposalKeypair])
+      .rpc();
 
+    return proposalKeypair;
+  }
+
+  it("Creates a proposal", async () => {
     try {
-      await program.methods
-        .createProposal(title, description, new anchor.BN(duration))
-        .accounts({
-          proposal: proposalKeypair.publicKey,
-          creator: provider.wallet.publicKey,
-          systemProgram: anchor.web3.SystemProgram.programId,
-        })
-        .signers([proposalKeypair])
-        .rpc();
+      const proposalKeypair = await createProposal();
 
       const proposalAccount = await program.account.proposal.fetch(
         proposalKeypair.publicKey
       );
 
-      expect(proposalAccount.title).to.equal(title);
-      expect(proposalAccount.description).to.equal(description);
+      expect(proposalAccount.title).to.equal("Test Proposal");
+      expect(proposalAccount.description).to.equal("Test Description");
       expect(proposalAccount.votesFor.toNumber()).to.equal(0);
       expect(proposalAccount.votesAgainst.toNumber()).to.equal(0);
-      expect(proposalAccount.creator.toBase58()).to.equal(
-        provider.wallet.publicKey.toBase58()
-      );
-
-      const now = Math.floor(Date.now() / 1000);
-      expect(proposalAccount.endsAt.toNumber()).to.be.approximately(
-        now + duration,
-        5
-      );
 
     } catch (error) {
       console.error("Error:", error);
       throw error;
+    }
+  });
+
+  async function createVoteAccount(
+    proposalPublicKey: anchor.web3.PublicKey,
+    voterPublicKey: anchor.web3.PublicKey
+  ) {
+    return anchor.web3.PublicKey.findProgramAddressSync(
+      [
+        Buffer.from("vote"),
+        proposalPublicKey.toBuffer(),
+        voterPublicKey.toBuffer(),
+      ],
+      program.programId
+    )[0];
+  }
+
+  it("Cat vote for a proposal", async () => {
+    const proposalKeypair = await createProposal();
+
+    const voteAccount = await createVoteAccount(
+      proposalKeypair.publicKey,
+      provider.wallet.publicKey
+    );
+
+    await program.methods.voteFor().accounts({
+      proposal: proposalKeypair.publicKey,
+      vote: voteAccount,
+      voter: provider.wallet.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    }).rpc();
+
+    const proposalAccount = await program.account.proposal.fetch(
+      proposalKeypair.publicKey
+    );
+    expect(proposalAccount.votesFor.toNumber()).to.equal(1);
+  });
+
+  it("Cannot vote after deadline", async () => {
+    const proposalKeypair = await createProposal(
+      "Quick vote",
+      "Test",
+      0.1 // 0.1 sec 
+    );
+
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    const voteAccount = await createVoteAccount(
+      proposalKeypair.publicKey,
+      provider.wallet.publicKey
+    );
+
+    try {
+      await program.methods
+        .voteFor()
+        .accounts({
+          proposal: proposalKeypair.publicKey,
+          voter: provider.wallet.publicKey,
+          vote: voteAccount,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        })
+        .rpc();
+
+      assert.fail("Should have thrown an error");
+    } catch (error) {
+      expect(error.message).to.include("Voting has ended");
+    }
+  });
+
+  it("Can vote against a proposal", async () => {
+    const proposalKeypair = await createProposal();
+
+    const voteAccount = await createVoteAccount(
+      proposalKeypair.publicKey,
+      provider.wallet.publicKey
+    );
+
+    await program.methods.voteAgainst().accounts({
+      proposal: proposalKeypair.publicKey,
+      vote: voteAccount,
+      voter: provider.wallet.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    }).rpc();
+
+    const proposalAccount = await program.account.proposal.fetch(
+      proposalKeypair.publicKey
+    );
+    expect(proposalAccount.votesAgainst.toNumber()).to.equal(1);
+    expect(proposalAccount.votesFor.toNumber()).to.equal(0);
+  });
+
+  it("Cannot vote twice on the same proposal", async () => {
+    const proposalKeypair = await createProposal();
+    const voteAccount = await createVoteAccount(
+      proposalKeypair.publicKey,
+      provider.wallet.publicKey
+    );
+
+    await program.methods.voteAgainst().accounts({
+      proposal: proposalKeypair.publicKey,
+      vote: voteAccount,
+      voter: provider.wallet.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    }).rpc();
+
+    try {
+      // try to vote again
+      await program.methods.voteAgainst().accounts({
+        proposal: proposalKeypair.publicKey,
+        vote: voteAccount,
+        voter: provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      }).rpc();
+
+      assert.fail("Should have thrown an error");
+    } catch (error) {
+      expect(error.message).to.include("already in use");
     }
   });
 });
